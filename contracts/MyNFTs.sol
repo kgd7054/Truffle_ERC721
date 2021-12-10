dab941d30edf572705015e03afb44f1d66a4688d
kgd7054 <kgd7054@gmail.com>
2021-12-11 02:29:56 +0900

    BEB_01_김기대Truffle_ERC721

---
+++ contracts/MyNFTs.sol
@@ -0,0 +1,28 @@
+// SPDX-LICENSE-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFTs is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() public ERC721("MyNFTs", "MNFT") {}
+
+    function mintNFT(string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
